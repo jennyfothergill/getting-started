ee8e283d4df8adbe22b83af0a0681a21b9435b93
Mike Henry <mikehenry@boisestate.edu>
2017-05-17 16:51:38 -0600

    checks for os now, removes backend if linux

--- config_files/safeCopy.sh
+++ config_files/safeCopy.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 mkdir -p ${HOME}/.vim/colors
 cp -i ir_black.vim ${HOME}/.vim/colors/.
 cp -i vmdrc ${HOME}/.vmdrc
 cp -i vimrc ${HOME}/.vimrc
-mkdir ${HOME}/.matplotlib
-cp -i matplotlibrc ${HOME}/.matplotlib/
+# http://stackoverflow.com/a/17072017
+if [ "$(uname)" == "Darwin" ]; then
+    mkdir ${HOME}/.matplotlib
+    cp -i matplotlibrc ${HOME}/.matplotlib/
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    #Let's use sed to remove the backend line, total hack -mmh
+    sed '1d' matplotlibrc > tmpfile
+    mv tmpfile matplotlibrc
+    mkdir -p ${HOME}/.config/matplotlib/
+    cp -i matplotlibrc ${HOME}/.config/matplotlib/matplotlibrc
+fi
