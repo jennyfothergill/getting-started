9e175d2d4d4d896acfc445d4de100bd60db10b80
Mike Henry <mikehenry@boisestate.edu>
2017-05-17 16:29:12 -0600

    removed python path lines and the auto copy of bash_profile

--- config_files/safeCopy.sh
+++ config_files/safeCopy.sh
@@ -2,7 +2,6 @@
 
 mkdir -p ${HOME}/.vim/colors
 cp -i ir_black.vim ${HOME}/.vim/colors/.
-cp -i bash_profile ${HOME}/.bash_profile
 cp -i vmdrc ${HOME}/.vmdrc
 cp -i vimrc ${HOME}/.vimrc
 mkdir ${HOME}/.matplotlib
